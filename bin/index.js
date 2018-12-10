2caf6f7b5a9c08f16d3939ddfbbd98c9674a6450
LiShuxue <1149926505@qq.com>
2018-12-10 16:57:57 +0800

    use inquirer to let user input or make choice

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,63 @@
 
 // commander 命令行参数解析
 const program = require('commander');
+const inquirer = require('inquirer');
+
+const interaction = function () {
+    console.log('start ...');
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'list',
+            message: 'Please choose from below list',
+            choices: ['Option1', 'Option2','Option3'],
+            filter: function (val) {
+                return val.toLowerCase();
+            }
+        },
+        {
+            type: 'rawlist',
+            name: 'rawList',
+            message: 'Please choose from below list with line number',
+            choices: ['Option1', 'Option2','Option3']
+        },
+        {
+            type: 'checkbox',
+            name: 'checkbox',
+            message: 'Please choose from checkbox',
+            choices: ['Option1', 'Option2','Option3', 'Option4'],
+            default: function() {
+                return ['Option1'];
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirm',
+            message: 'Do you want to continue ?',
+            default: function() {
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'input',
+            message: "What's your name ?",
+            default: function() {
+                return 'LSX';
+            }
+        },
+        {
+            type: 'password',
+            name: 'password',
+            message: 'Enter a password',
+            mask: '*'
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+        console.log('end ...');
+    });
+}
 
 program
     .version('1.0.0')
@@ -20,6 +77,13 @@ program
     .action(function (msg) {
         console.log('this is the second demo, this is the msg: ' + msg);
     });
+
+program
+    .command('demo3')
+    .description('run the third demo, interact with the user')
+    .action(function () {
+        interaction();
+    })
 
 program.parse(process.argv);
 
