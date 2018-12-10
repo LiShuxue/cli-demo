9e8c4d162be1c4dd1352f198c73259b2ce44bd3e
LiShuxue <1149926505@qq.com>
2018-12-10 16:14:00 +0800

    use commander to receive the args

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log('Hello world!');
+// commander 命令行参数解析
+const program = require('commander');
+
+program
+    .version('1.0.0')
+    .option('-t, --test <testmessage>', 'output your test message')
+
+program
+    .command('demo1')
+    .description('run the first demo')
+    .action(function () {
+        console.log('this is the first demo');
+    });
+
+program
+    .command('demo2 <msg>')
+    .description('run the second demo')
+    .action(function (msg) {
+        console.log('this is the second demo, this is the msg: ' + msg);
+    });
+
+program.parse(process.argv);
+
+if (program.test) {
+    console.log(program.test);
+}
